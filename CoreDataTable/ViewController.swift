c106393eda3ca78f9a63727772c8c2e94f115966
Unknown <kosyak_@mail.ru>
2018-07-10 11:21:02 +0300

    Save and fetch names using CoreData and KVC

--- CoreDataTable/ViewController.swift
+++ CoreDataTable/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var names: [String] = []
+    //NSManagedObject represents a single object stored in Core Data; you must use it to create, edit, save and delete from your Core Data persistent store
+    var people: [NSManagedObject] = [] //store Person entities
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +23,26 @@ class ViewController: UIViewController {
         tableView.register(UITableViewCell.self,
                            forCellReuseIdentifier: "Cell")
         
+    }
+    
+    //Fetching from Core Data
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //1
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // fetches all objects of a particular entity
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+        
+        //fetch(_:) returns an array of managed objects meeting the criteria specified by the fetch request
+        do {
+            people = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
     }
     
     //Present an alert to add new name
@@ -32,7 +54,7 @@ class ViewController: UIViewController {
         let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] action in
             guard let textField = alert.textFields?.first,
             let nameToSave = textField.text else { return }
-                self.names.append(nameToSave)
+                self.save(name: nameToSave)
                 self.tableView.reloadData()
             }
         
@@ -46,6 +68,30 @@ class ViewController: UIViewController {
         present(alert, animated: true)
     }
     
+    func save(name: String) {
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        // insert a new managed object into a managed object context
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // create a new managed object and insert it into the managed object context
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
+        
+        let person = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        // With an NSManagedObject in hand, you set the name attribute using key-value coding. You must spell the KVC key (name in this case) exactly as it appears in your Data Model, otherwise your app will crash at runtime
+        person.setValue(name, forKeyPath: "name")
+        
+        // commit your changes to person and save to disk by calling save on the managed object context. Note save can throw an error, which is why you call it using the try keyword within a do-catch block. Finally, insert the new managed object into the people array so it shows up when the table view reloads
+        do {
+            try managedContext.save()
+            people.append(person)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
 
 }
 
@@ -54,14 +100,17 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return people.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-            cell.textLabel?.text = names[indexPath.row]
-            return cell
+        let person = people[indexPath.row]
+        
+        //grab the name attribute from the NSManagedObject
+        cell.textLabel?.text = person.value(forKeyPath: "name") as? String
+        return cell
     }
 }
 
